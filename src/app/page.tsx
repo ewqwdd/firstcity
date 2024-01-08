0fd68826e008957123ddf84ed1205a7a114e6d7d
Mykolai Skydan <117273969+ewqwdd@users.noreply.github.com>
2024-01-08 16:23:06 +0000

    add input

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,5 @@
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
 import HStack from "@/shared/ui/Flex/HStack";
 import VStack from "@/shared/ui/Flex/VStack";
 import Heading from "@/shared/ui/Heading";
@@ -9,7 +11,7 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <VStack>
+    <VStack className="gap-4">
       <div className="w-44 h-44 relative rounded-full overflow-clip self-center pointer-events-none">
         <Image alt="Person sitting on the chair." src='/images/person_sitting.png' fill objectFit='cover'/>
       </div>
@@ -26,8 +28,29 @@ export default function Home() {
         <Paragraph fontFamily="Salsa" className="text-slate-600">
           Finding care simplified
         </Paragraph>
-        <CardsCarousel />
       </VStack>
+        <CardsCarousel />
+        <HStack className="items-center">
+          <Button variant={"default"}>
+            Button
+          </Button>
+          <Button variant={"destructive"}>
+            Button
+          </Button>
+          <Button variant={"ghost"}>
+            Button
+          </Button>
+          <Button variant={"outline"}>
+            Button
+          </Button>
+          <Button variant={"secondary"}>
+            Button
+          </Button>
+          <Button variant={"transparent"}>
+            Button
+          </Button>
+        </HStack>
+        <Input className="self-center w-44"/>
     </VStack>
   )
 }
